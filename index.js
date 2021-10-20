e7066ef23fe0444e712bbc3b7f86df3d955afb59
Anca <ancutafarcas@yahoo.com>
2021-10-20 09:29:13 +0300

    Anca

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 let loggedUser = 1; // utilizatorul conectat in aplicatie
 let users = [ // lista completa de jucatori
     { id: 1, email: 'viorelfilip@outlook.com' },
@@ -33,18 +34,26 @@ function showLoggedUser() {
 function showGames() {
     let game = games[0];
     let el = document.getElementById('game');
+    let el1 = document.getElementById('text');
     let opUser = (game.idUser1 === loggedUser ? game.idUser2 : game.idUser1);
     let email = users.filter(u => u.id === opUser)[0].email;
-    el.innerHTML = `<p>${email}</p>`;
-    el.innerHTML += `<div class="game-cell">${game.c1 || ''}</div>`;
-    el.innerHTML += `<div class="game-cell">${game.c2 || ''}</div>`;
-    el.innerHTML += `<div class="game-cell">${game.c3 || ''}</div>`;
-    el.innerHTML += `<div class="game-cell">${game.c4 || ''}</div>`;
-    el.innerHTML += `<div class="game-cell">${game.c5 || ''}</div>`;
-    el.innerHTML += `<div class="game-cell">${game.c6 || ''}</div>`;
-    el.innerHTML += `<div class="game-cell">${game.c7 || ''}</div>`;
-    el.innerHTML += `<div class="game-cell">${game.c8 || ''}</div>`;
-    el.innerHTML += `<div class="game-cell">${game.c9 || ''}</div>`;
+    el1.innerHTML = `<p>${email}</p>`;
+    el.innerHTML += `<div class="game-cell" onclick="Logic('1')">${game.c1 || ''}</div>`;
+    el.innerHTML += `<div class="game-cell" onclick="Logic('2')">${game.c2 || ''}</div>`;
+    el.innerHTML += `<div class="game-cell" onclick="Logic('3')">${game.c3 || ''}</div>`;
+    el.innerHTML += `<div class="game-cell" onclick="Logic('4')">${game.c4 || ''}</div>`;
+    el.innerHTML += `<div class="game-cell" onclick="Logic('5')">${game.c5 || ''}</div>`;
+    el.innerHTML += `<div class="game-cell" onclick="Logic('6')">${game.c6 || ''}</div>`;
+    el.innerHTML += `<div class="game-cell" onclick="Logic('7')">${game.c7 || ''}</div>`;
+    el.innerHTML += `<div class="game-cell" onclick="Logic('8')">${game.c8 || ''}</div>`;
+    el.innerHTML += `<div class="game-cell" onclick="Logic('9')">${game.c9 || ''}</div>`;
 }
+
+function Logic(a){
+        alert(a);
+}
+
+
+
 
 
