f39cd392d6f55a9f85c4618f3f62a28a7c087708
Emeric8 <emeric.lacatus24@gmail.com>
2021-10-20 10:17:28 +0300

    Commit

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ let games = [ // doar jocurile utilizatorului conectat
 function showData() {
     showGames();
     showLoggedUser();
-    // clickOnCell();
+    clickOnCell();
 }
 
 function showLoggedUser() {
@@ -37,49 +37,8 @@ function showGames() {
     let players = document.getElementsByClassName("players")[0];
     let opUser = (game.idUser1 === loggedUser ? game.idUser2 : game.idUser1);
     let email = users.filter(u => u.id === opUser)[0].email;
-    players.innerHTML = `<p> Player 2: ${email}</p>`;
     el.innerHTML = `<p>${email}</p>`;
-    // el.innerHTML += `<div class="game-cell">${game.c1 || ''}</div>`;
-    // el.innerHTML += `<div class="game-cell">${game.c2 || ''}</div>`;
-    // el.innerHTML += `<div class="game-cell">${game.c3 || ''}</div>`;
-    // el.innerHTML += `<div class="game-cell">${game.c4 || ''}</div>`;
-    // el.innerHTML += `<div class="game-cell">${game.c5 || ''}</div>`;
-    // el.innerHTML += `<div class="game-cell">${game.c6 || ''}</div>`;
-    // el.innerHTML += `<div class="game-cell">${game.c7 || ''}</div>`;
-    // el.innerHTML += `<div class="game-cell">${game.c8 || ''}</div>`;
-    // el.innerHTML += `<div class="game-cell">${game.c9 || ''}</div>`;
-    
-    el.innerHTML += `<div id="c1" onClick="clickOnCell(this)" class="game-cell">${game.c1 || ''}</div>`;
-    el.innerHTML += `<div id="c2" onClick="clickOnCell(this)" class="game-cell">${game.c2 || ''}</div>`;
-    el.innerHTML += `<div id="c3" onClick="clickOnCell(this)" class="game-cell">${game.c3 || ''}</div>`;
-    el.innerHTML += `<div id="c4" onClick="clickOnCell(this)" class="game-cell">${game.c4 || ''}</div>`;
-    el.innerHTML += `<div id="c5" onClick="clickOnCell(this)" class="game-cell">${game.c5 || ''}</div>`;
-    el.innerHTML += `<div id="c6" onClick="clickOnCell(this)" class="game-cell">${game.c6 || ''}</div>`;
-    el.innerHTML += `<div id="c7" onClick="clickOnCell(this)" class="game-cell">${game.c7 || ''}</div>`;
-    el.innerHTML += `<div id="c8" onClick="clickOnCell(this)" class="game-cell">${game.c8 || ''}</div>`;
-    el.innerHTML += `<div id="c9" onClick="clickOnCell(this)" class="game-cell">${game.c9 || ''}</div>`;
 
     
 }
 
-let currentPlayer = 1;
-
-function clickOnCell(cell){
-    //let cell = document.getElementById('game');
-    console.log(cell.id);
-    if(cell.innerHTML === 'X' || cell.innerHTML === 'O'){
-       return;
-    }
-    let symbol = "";
-    if(currentPlayer == 1){
-        symbol = "X";
-        currentPlayer = 2;
-    } else {
-        symbol = "O";
-        currentPlayer = 1;
-    }
-    
-    cell.innerHTML = symbol;
-    console.log("Cell was clicked!");
-
-}
