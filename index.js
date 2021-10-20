33da821424f68ad1319a8f5df06acac399697082
patriciarocas <patricia_rocas@yahoo.com>
2021-10-20 09:12:29 +0300

    test1

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let users = [ // lista completa de jucatori
     { id: 2, email: 'emeric.lacatus@gmail.com' }
 ];
 
+
 let games = [ // doar jocurile utilizatorului conectat
     {
         id: 1,
@@ -12,10 +13,10 @@ let games = [ // doar jocurile utilizatorului conectat
         c1: null,
         c2: null,
         c3: null,
-        c4: 'x',
-        c5: '0',
+        c4: 'X',
+        c5: 'O',
         c6: null,
-        c7: 'x',
+        c7: 'X',
         c8: null,
         c9: null
     }
@@ -24,27 +25,63 @@ let games = [ // doar jocurile utilizatorului conectat
 function showData() {
     showGames();
     showLoggedUser();
+
 }
 
 function showLoggedUser() {
     let el = document.getElementsByClassName("loggedUser")[0];
-    el.innerHTML = 'Salut, ' + users.filter(u => u.id === loggedUser)[0].email;
+    el.innerHTML = 'Player 1: ' + users.filter(u => u.id === loggedUser)[0].email;
+
+ 
 }
+
 function showGames() {
     let game = games[0];
     let el = document.getElementById('game');
+    let players = document.getElementsByClassName("players")[0];
+
+
     let opUser = (game.idUser1 === loggedUser ? game.idUser2 : game.idUser1);
     let email = users.filter(u => u.id === opUser)[0].email;
-    el.innerHTML = `<p>${email}</p>`;
-    el.innerHTML += `<div class="game-cell">${game.c1 || ''}</div>`;
-    el.innerHTML += `<div class="game-cell">${game.c2 || ''}</div>`;
-    el.innerHTML += `<div class="game-cell">${game.c3 || ''}</div>`;
-    el.innerHTML += `<div class="game-cell">${game.c4 || ''}</div>`;
-    el.innerHTML += `<div class="game-cell">${game.c5 || ''}</div>`;
-    el.innerHTML += `<div class="game-cell">${game.c6 || ''}</div>`;
-    el.innerHTML += `<div class="game-cell">${game.c7 || ''}</div>`;
-    el.innerHTML += `<div class="game-cell">${game.c8 || ''}</div>`;
-    el.innerHTML += `<div class="game-cell">${game.c9 || ''}</div>`;
+    players.innerHTML = `<p> Player 2: ${email}</p>`;
+    
+  
+    el.innerHTML += `<div id="c1" onClick="clickCell(this)" class="game-cell">${game.c1 || ''}</div>`;
+    el.innerHTML += `<div id="c2" onClick="clickCell(this)" class="game-cell">${game.c2 || ''}</div>`;
+    el.innerHTML += `<div id="c3" onClick="clickCell(this)" class="game-cell">${game.c3 || ''}</div>`;
+    el.innerHTML += `<div id="c4" onClick="clickCell(this)" class="game-cell">${game.c4 || ''}</div>`;
+    el.innerHTML += `<div id="c5" onClick="clickCell(this)" class="game-cell">${game.c5 || ''}</div>`;
+    el.innerHTML += `<div id="c6" onClick="clickCell(this)" class="game-cell">${game.c6 || ''}</div>`;
+    el.innerHTML += `<div id="c7" onClick="clickCell(this)" class="game-cell">${game.c7 || ''}</div>`;
+    el.innerHTML += `<div id="c8" onClick="clickCell(this)" class="game-cell">${game.c8 || ''}</div>`;
+    el.innerHTML += `<div id="c9" onClick="clickCell(this)" class="game-cell">${game.c9 || ''}</div>`;
+
+    
 }
+
+let currPlayer = 1;
+
+function clickCell(cell){
+    //let cell = document.getElementById('game');
+    console.log(cell.id);
+    if(cell.innerHTML === 'X' || cell.innerHTML === 'O'){
+       return;
+    }
+    let symbol = "";
+    if(currPlayer == 1){
+        symbol = "X";
+        currPlayer = 2;
+    } else {
+        symbol = "O";
+        currPlayer = 1;
+    }
+    
+    cell.innerHTML = symbol;
+    console.log("Cell was clicked!");
+
+}
+
+
+
 
 
