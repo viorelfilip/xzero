eab4c34d88ddb3f35a68274360a140d71c72b282
Viorel Filip <viorelfilip@outlook.com>
2021-10-21 05:13:18 +0300

    mysql api even better

--- api/mysql.php
+++ api/mysql.php
@@ -5,7 +5,7 @@
         private static $conn;
         private static $host="gamedb.mysql.database.azure.com";
         private static $user="viorelfilip@gamedb";
-        private static $pass="Indeco@2020-";
+        private static $pass="Indeco@2020";
         private static $db="xzero";
         private static $port=3306;
 
@@ -22,22 +22,34 @@
             }
         }
         // method declaration
-        public static function query($sql)
+        public static function query($sql, $params=null)
         {
+            $rows = array();
+            $result = null;
             /* check connection */
             if (mysqli_connect_errno()) {
                 $errMsg = mysqli_connect_error();
                 header("HTTP/1.1 500 mysql::connect(): $errMsg");
                 exit();
             }
-            if ($result = mysqli_query(self::$conn, $sql)) {
-                $rows = array();
-                while ($r = mysqli_fetch_assoc($result)) {
-                    $rows[] = $r;
+            if (!empty($params)) {
+                $stmt = self::$conn->prepare($sql);
+                foreach ($params as $p) {
+                    $stmt->bind_param('s', $p); // bind all as string
                 }
-                mysqli_free_result($result); // free the result set
-                print json_encode($rows); // return as json array
+                $stmt->execute();
+                $result = $stmt->get_result();
+            } else {
+                $result = mysqli_query(self::$conn, $sql);
             }
+            while ($row = $result->fetch_assoc()) {
+                $rows[] = $row;
+            }
+            mysqli_free_result($result); // free the result set
+            if (!empty($stmt)) {
+                $stmt->close();
+            }
+            print json_encode($rows); // return as json array
         }
     }
     mysql::connect();
--- api/query.php
+++ api/query.php
@@ -1,17 +1,45 @@
 <?php
 include_once 'mysql.php';
 
-$users = 'select * from users';
-$games = 'select * from games';
+$queries = array( // declare here all the queries
+    'users-all'      => 'select * from users',
+    'games-all'      => 'select * from games',
+    'games-by-user'  => 'select * from games where ? in (idUser1, idUser2)',
+    'new-game'       => 'insert into games(idUser1, idUser2) values(?, ?)',
+    'new-user'       => 'insert into users(email,password) values(?, ?)',
+    'set-game-by-id' => 'update games set c1=?,c2=?,c3=?,c4=?,c5=?,c6=?,c7=?,c8=?,c9=? where id=?',
+    'set-game-c1'    => 'update games set c1=? where id=?',
+    'set-game-c2'    => 'update games set c2=? where id=?',
+    'set-game-c3'    => 'update games set c3=? where id=?',
+    'set-game-c4'    => 'update games set c4=? where id=?',
+    'set-game-c5'    => 'update games set c5=? where id=?',
+    'set-game-c6'    => 'update games set c6=? where id=?',
+    'set-game-c7'    => 'update games set c7=? where id=?',
+    'set-game-c8'    => 'update games set c8=? where id=?',
+    'set-game-c9'    => 'update games set c9=? where id=?'
+);
 
 if (!empty($argv)) { // for debug scenario, command line get params
     parse_str(implode('&', array_slice($argv, 1)), $_GET);
 }
 
-if (!(array_key_exists("table", $_GET))) {
-    header('HTTP/1.1 400 Bad Request {"table": "param <table> is required"}');
+if (!(array_key_exists("query", $_GET))) {
+    header('HTTP/1.1 400 Bad Request {"query": "param <query> is required"}');
     exit;
 }
 
-$query = (array_key_exists("table", $_GET)) ? $_GET["table"]: "";
-mysql::query($$query);
+while ($query = current($queries)) {
+    if (key($queries) == $_GET["query"]) {
+        if (strpos($query, '?') !== false) {
+            array_shift($_GET);
+            mysql::query($query, $_GET);
+        } else {
+            mysql::query($query);
+        }
+        exit();
+    }
+    next($queries);
+}
+
+// $query = $_GET["query"];
+// mysql::query($query);
