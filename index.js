363570ff84f6574ce875e00384332ffe0556ee5a
Anca <ancutafarcas@yahoo.com>
2021-10-21 09:14:57 +0300

    reset

--- index.js
+++ index.js
@@ -13,14 +13,35 @@ let games = [ // doar jocurile utilizatorului conectat
         c1: null,
         c2: null,
         c3: null,
-        c4: 'x',
-        c5: '0',
+        c4: null,
+        c5: null,
         c6: null,
-        c7: 'x',
+        c7: null,
         c8: null,
         c9: null
     }
 ];
+
+function gameOver(){
+    let game = games[0];
+    let wins = ['OOO', 'XXX']
+    if(~wins.indexOf(game.c1+game.c2+game.c3))
+        return alert('Gata joc');
+    if(~wins.indexOf(game.c4+game.c5+game.c6))
+        return alert('Gata joc');
+    if(~wins.indexOf(game.c7+game.c8+game.c9))
+        return alert('Gata joc');
+    if(~wins.indexOf(game.c1+game.c4+game.c7))
+        return alert('Gata joc');
+    if(~wins.indexOf(game.c2+game.c5+game.c8))
+        return alert('Gata joc');
+    if(~wins.indexOf(game.c3+game.c6+game.c9))
+        return alert('Gata joc');
+    if(~wins.indexOf(game.c1+game.c5+game.c9))
+        return alert('Gata joc');
+    if(~wins.indexOf(game.c3+game.c5+game.c7))
+        return alert('Gata joc');
+}
 
 function showData() {
     showGames();
@@ -29,48 +50,70 @@ function showData() {
 
 function showLoggedUser() {
     let el = document.getElementsByClassName("loggedUser")[0];
-    el.innerHTML = 'Salut, ' + users.filter(u => u.id === loggedUser)[0].email;
+    el.innerHTML = 'Player 1: ' + users.filter(u => u.id === loggedUser)[0].email;
 }
 function showGames() {
     let game = games[0];
     let el = document.getElementById('game');
-    let el1 = document.getElementById('text');
+    let players = document.getElementById("players");
     
     let opUser = (game.idUser1 === loggedUser ? game.idUser2 : game.idUser1);
     let email = users.filter(u => u.id === opUser)[0].email;
-    el1.innerHTML = `<p>${email}</p>`;
-    el.innerHTML += `<div id="c1" onclick="Logic('this')" class="game-cell">${game.c1 || ''}</div>`;
-    el.innerHTML += `<div id="c2" onclick="Logic('this')" class="game-cell" >${game.c2 || ''}</div>`;
-    el.innerHTML += `<div id="c3" onclick="Logic('this')" class="game-cell">${game.c3 || ''}</div>`;
-    el.innerHTML += `<div id="c4" onclick="Logic('this')" class="game-cell">${game.c4 || ''}</div>`;
-    el.innerHTML += `<div id="c5" onclick="Logic('this')" class="game-cell">${game.c5 || ''}</div>`;
-    el.innerHTML += `<div id="c6" onclick="Logic('this')" class="game-cell">${game.c6 || ''}</div>`;
-    el.innerHTML += `<div id="c7" onclick="Logic('this')" class="game-cell">${game.c7 || ''}</div>`;
-    el.innerHTML += `<div id="c8" onclick="Logic('this')" class="game-cell">${game.c8 || ''}</div>`;
-    el.innerHTML += `<div id="c9" onclick="Logic('this')" class="game-cell">${game.c9 || ''}</div>`;
+    players.innerHTML = `<p> Player 2: ${email}</p>`;
+    el.innerHTML="";
+    el.innerHTML += `<div id="c1" class="game-cell" onclick="Logic(this)">${game.c1 || ''}</div>`;
+    el.innerHTML += `<div id="c2" class="game-cell" onclick="Logic(this)">${game.c2 || ''}</div>`;
+    el.innerHTML += `<div id="c3" class="game-cell" onclick="Logic(this)">${game.c3 || ''}</div>`;
+    el.innerHTML += `<div id="c4" class="game-cell" onclick="Logic(this)">${game.c4 || ''}</div>`;
+    el.innerHTML += `<div id="c5" class="game-cell" onclick="Logic(this)">${game.c5 || ''}</div>`;
+    el.innerHTML += `<div id="c6" class="game-cell" onclick="Logic(this)">${game.c6 || ''}</div>`;
+    el.innerHTML += `<div id="c7" class="game-cell" onclick="Logic(this)">${game.c7 || ''}</div>`;
+    el.innerHTML += `<div id="c8" class="game-cell" onclick="Logic(this)">${game.c8 || ''}</div>`;
+    el.innerHTML += `<div id="c9" class="game-cell" onclick="Logic(this)">${game.c9 || ''}</div>`;
 }
 
-let currPlayer = 1;
+let currentPlayer = 1;
 
-function Logic(a){
-       /* alert(a);*/
-        console.log(cell.id);
+function Logic(cell){
+       //alert(a);
+        console.log(cell);
         if(cell.innerHTML === 'X' || cell.innerHTML === 'O'){
            return;
         }
         let symbol = "";
-        if(currPlayer == 1){
+        if(currentPlayer == 1){
             symbol = "X";
-            currPlayer = 2;
+            currentPlayer = 2;
         } else {
             symbol = "O";
-            currPlayer = 1;
+            currentPlayer = 1;
         }
         
-        cell.innerHTML = symbol;
-        console.log("Cell was clicked!");
+    cell.innerHTML = symbol;
+    games[0][cell.id] = symbol;
+    gameOver();
     
 }
+
+function resetGame(){
+    currentPlayer=1;
+    for(let i=0;i<=8;i++)
+        document.getElementsByClassName('game-cell')[i].innerHTML="";
+    games[0].c1=null;
+    games[0].c2=null;
+    games[0].c3=null;
+    games[0].c4=null;
+    games[0].c5=null;
+    games[0].c6=null;
+    games[0].c7=null;
+    games[0].c8=null;
+    games[0].c9=null;
+
+}
+
+
+
+
 
 
 
