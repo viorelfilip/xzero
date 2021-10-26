83cba63262e7e9825978b1c84d6fb5d07b8fad49
Anca <ancutafarcas@yahoo.com>
2021-10-26 18:05:26 +0300

    test5

--- index.js
+++ index.js
@@ -1,14 +1,65 @@
-let loggedUser = 1; // utilizatorul conectat in aplicatie
+import { getPlayers, saveMove, saveReset, gamesByUser } from '/xzero/data.js';
+
+let loggedUserId = 2; // utilizatorul conectat in aplicatie
 let users = [ // lista completa de jucatori
     { id: 1, email: 'viorelfilip@outlook.com' },
-    { id: 2, email: 'emeric.lacatus@gmail.com' }
+    { id: 2, email: 'emeric.lacatus@gmail.com' },
+    { id: 3, email: 'ioana.pop@gmail.com' },
+    { id: 4, email: 'ionut.popescu@gmail.com' },
+    { id: 5, email: 'ana.ionescu@gmail.com' }
 ];
+
+let conf = {
+    cells: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    ocolor: 'lightskyblue',
+    xcolor: 'lightsalmon',
+    dcolor: 'azure',
+    winsound: new Audio("win-sound.mp3"),
+    defsound: new Audio("def-sound.mp3")
+}
 
 let games = [ // doar jocurile utilizatorului conectat
     {
         id: 1,
         idUser1: 1,
         idUser2: 2,
+        scorUser1: 0,
+        scorUser2: 0,
+        nextMove: "O",
+        c1: "X",
+        c2: null,
+        c3: null,
+        c4: null,
+        c5: "O",
+        c6: null,
+        c7: null,
+        c8: null,
+        c9: null
+    },
+    {
+        id: 2,
+        idUser1: 1,
+        idUser2: 3,
+        scorUser1: 0,
+        scorUser2: 0,
+        nextMove: "X",
+        c1: 'O',
+        c2: null,
+        c3: null,
+        c4: null,
+        c5: 'X',
+        c6: null,
+        c7: null,
+        c8: 'X',
+        c9: null
+    },
+    {
+        id: 3,
+        idUser1: 1,
+        idUser2: 4,
+        scorUser1: 0,
+        scorUser2: 0,
+        nextMove: "O",
         c1: null,
         c2: null,
         c3: null,
@@ -21,110 +72,184 @@ let games = [ // doar jocurile utilizatorului conectat
     }
 ];
 
-var audio = new Audio('sound.wav'); 
+export function loadGames() {
+    gamesByUser(loggedUserId)
+        .then(response => response.json())
+        .then(data => {
+            games = data;
 
-function gameOver(){
-    let game = games[0];
-    let wins = ['OOO', 'XXX']
-    if(~wins.indexOf(game.c1+game.c2+game.c3))
-        return alert('Gata joc');
-    if(~wins.indexOf(game.c4+game.c5+game.c6))
-        return alert('Gata joc');
-    if(~wins.indexOf(game.c7+game.c8+game.c9))
-        return alert('Gata joc');
-    if(~wins.indexOf(game.c1+game.c4+game.c7))
-        return alert('Gata joc');
-    if(~wins.indexOf(game.c2+game.c5+game.c8))
-        return alert('Gata joc');
-    if(~wins.indexOf(game.c3+game.c6+game.c9))
-        return alert('Gata joc');
-    if(~wins.indexOf(game.c1+game.c5+game.c9))
-        return alert('Gata joc');
-    if(~wins.indexOf(game.c3+game.c5+game.c7))
-        return alert('Gata joc');
-    audio.play();
-}
-
-
-function showData() {
-    showGames();
-    showLoggedUser();
+            getPlayers()
+                .then(response => response.json())
+                .then(data => {
+                    users = data;
+                    showGames();
+                    showLoggedUser();
+                })
+            console.warn(data);
+        });
 }
 
 function showLoggedUser() {
     let el = document.getElementsByClassName("loggedUser")[0];
-    el.innerHTML = 'Player 1: ' + users.filter(u => u.id === loggedUser)[0].email;
+    el.innerHTML = 'Player: ' + users.filter(u => u.id === loggedUserId)[0].email;
 }
+
+function grid(game) {
+    let idx = games.indexOf(game);
+    let el = document.createElement('div');
+    el.className = "container px-0";
+    //config cell 
+    conf.cells.forEach(i => {
+        let propValue = game[`c${i}`];
+        el.innerHTML += `<div id="c${i}" onClick="clickCell(this,${idx})" 
+        class="game-cell" style="background-color: ${propValue === null ? conf.dcolor : setColor(propValue)}">
+        ${propValue || ''} </div>`;
+    })
+
+    return el;
+
+}
+
 function showGames() {
-    let game = games[0];
-    let el = document.getElementById('game');
-    let players = document.getElementById("players");
-    
-    let opUser = (game.idUser1 === loggedUser ? game.idUser2 : game.idUser1);
-    let email = users.filter(u => u.id === opUser)[0].email;
-    players.innerHTML = `<p> Player 2: ${email}</p>`;
-    el.innerHTML="";
-    el.innerHTML += `<div id="c1" class="game-cell" onclick="Logic(this)">${game.c1 || ''}</div>`;
-    el.innerHTML += `<div id="c2" class="game-cell" onclick="Logic(this)">${game.c2 || ''}</div>`;
-    el.innerHTML += `<div id="c3" class="game-cell" onclick="Logic(this)">${game.c3 || ''}</div>`;
-    el.innerHTML += `<div id="c4" class="game-cell" onclick="Logic(this)">${game.c4 || ''}</div>`;
-    el.innerHTML += `<div id="c5" class="game-cell" onclick="Logic(this)">${game.c5 || ''}</div>`;
-    el.innerHTML += `<div id="c6" class="game-cell" onclick="Logic(this)">${game.c6 || ''}</div>`;
-    el.innerHTML += `<div id="c7" class="game-cell" onclick="Logic(this)">${game.c7 || ''}</div>`;
-    el.innerHTML += `<div id="c8" class="game-cell" onclick="Logic(this)">${game.c8 || ''}</div>`;
-    el.innerHTML += `<div id="c9" class="game-cell" onclick="Logic(this)">${game.c9 || ''}</div>`;
+    for (let game of games) {
+        let idx = games.indexOf(game);
+        // let players = document.getElementsByClassName("players")[0];
+        let gameContainer = document.getElementById("gameContainer");
+        //let gameContainer = document.createElement('div');
+
+        let divState = document.createElement("div");
+        divState.className = "status";
+        divState.id = `game_${idx}`
+        //opp player
+        let opUser = (game.idUser1 === loggedUserId ? game.idUser2 : game.idUser1);
+        let email = users.filter(u => u.id === opUser)[0].email;
+        let current = document.createElement('p');
+        current.id = 'current';
+        setPlayerState(current, game, true);
+        let partner = document.createElement('p');
+        partner.id = 'partner';
+        setPlayerState(partner, game)
+
+        divState.appendChild(current);
+        divState.appendChild(partner);
+
+        divState.appendChild(grid(game));
+        gameContainer.appendChild(divState);
+
+        //button reset
+        divState.innerHTML += `<button id="btn${idx}" onClick="reset(${idx})" class="w-100 btn btn-lg btn-primary" disabled>
+        <i class="fa fa-fw fa-undo"></i> Reset</button>`;
+
+    }
+
 }
 
-let currentPlayer = 1;
+function setPlayerState(element, game, loggedUser = false) {
+    if (loggedUser) {
+        let scor = loggedUserId == game.idUser1 ? game.scorUser1 : game.scorUser2;
+        let myTurn = (game.nextMove == 'X' && game.userX == loggedUserId) ||
+            (game.nextMove == 'O' && game.userX != loggedUserId);
+        element.innerHTML = `${scor} > You ( ${myTurn ? 'My turn' : 'Wait'} )`;
+        element.className = myTurn ? "move-active" : "move-await";
+    } else {
+        let partner = users.filter(u => u.id == (game.idUser1 == loggedUserId ? game.idUser2 : game.idUser1))[0];
+        let scor = partner.id == game.idUser1 ? game.scorUser1 : game.scorUser2;
+        let yourTurn = (game.nextMove == 'X' && game.userX != loggedUserId) ||
+            (game.nextMove == 'O' && game.userX == loggedUserId);
+        element.innerHTML = `${scor} > ${partner.email} ( ${yourTurn ? 'Your turn' : 'Wait'} )`;
+        element.className = yourTurn ? "move-active" : "move-await";
+    }
+}
 
-function Logic(cell){
-       //alert(a);
-        console.log(cell);
-        if(cell.innerHTML === 'X' || cell.innerHTML === 'O'){
-           return;
-        }
-        let symbol = "";
-        if(currentPlayer == 1){
-            symbol = "X";
-            currentPlayer = 2;
-        } else {
-            symbol = "O";
-            currentPlayer = 1;
-        }
-        
+function playerWin(game) { //aici ar trebui sa verificam cine a castigat. id1 sau id2 pt afisare scor.
+    let wins = ['OOO', 'XXX'];
+    if (~wins.indexOf(game.c1 + game.c2 + game.c3)) {
+        scoreGame(game, game.c1);
+    }
+    if (~wins.indexOf(game.c1 + game.c4 + game.c7)) {
+        scoreGame(game, game.c1);
+    }
+    if (~wins.indexOf(game.c1 + game.c5 + game.c9)) {
+        scoreGame(game, game.c1);
+    }
+    if (~wins.indexOf(game.c2 + game.c5 + game.c8)) {
+        scoreGame(game, game.c2);
+    }
+    if (~wins.indexOf(game.c3 + game.c6 + game.c9)) {
+        scoreGame(game, game.c3);
+    }
+    if (~wins.indexOf(game.c3 + game.c5 + game.c7)) {
+        scoreGame(game, game.c3);
+    }
+    if (~wins.indexOf(game.c4 + game.c5 + game.c6)) {
+        scoreGame(game, game.c4);
+    }
+    if (~wins.indexOf(game.c7 + game.c8 + game.c9)) {
+        scoreGame(game, game.c7);
+    }
+    conf.defsound.play();
+}
+
+function reset(idx) {
+    let game = games[idx];
+    conf
+        .cells
+        .forEach(i => {
+            game[`c${i}`] = null;
+            let gameEl = document.querySelector(`#game_${idx}`);
+            let cellEl = gameEl.querySelector(`#c${i}`);
+            cellEl.innerHTML = '';
+            cellEl.innerText = '';
+            cellEl.style.backgroundColor = conf.dcolor;
+        })
+    saveReset(game.id);
+}
+
+function scoreGame(game, cellValue) { //jucatorul curent verificam daca joaca cu x sau cu o,
+    conf.winsound.play();             // in functie de id mergem in game si verificm daca id ul celui care a castigat sau a pierdut e egal cu jucatorul curent
+    if (cellValue === 'X') {
+        if (game.idUser1 == game.userX) game.scorUser1++;
+        else game.scorUser2++;
+    } else {
+        if (game.idUser1 == game.userX) game.scorUser2++;
+        else game.scorUser1++;
+    }
+
+    const gameId = games.indexOf(game);
+    document.getElementById(`btn${gameId}`).disabled = false;
+
+}
+
+function clickCell(cell, idx) {
+    if (cell.innerHTML === 'X' || cell.innerHTML === 'O') {
+        return;
+    }
+    let game = games[idx];
+    let symbol = game.nextMove;
     cell.innerHTML = symbol;
-    games[0][cell.id] = symbol;
-    gameOver();
-    
+    cell.style.backgroundColor = setColor(symbol);
+    game[cell.id] = symbol;
+    game[cell.id] = symbol;
+    playerWin(game);
+    game.nextMove = game.nextMove === 'X' ? 'O' : 'X';
+    saveMove(cell.id, symbol, game.id);
+    let gameEl = document.querySelector(`#game_${idx}`);
+    setPlayerState(gameEl.querySelector('#current'), game, true);
+    setPlayerState(gameEl.querySelector('#partner'), game);
 }
 
-function resetGame(){
-    currentPlayer=1;
-    for(let i=0;i<=8;i++)
-        document.getElementsByClassName('game-cell')[i].innerHTML="";
-    games[0].c1=null;
-    games[0].c2=null;
-    games[0].c3=null;
-    games[0].c4=null;
-    games[0].c5=null;
-    games[0].c6=null;
-    games[0].c7=null;
-    games[0].c8=null;
-    games[0].c9=null;
-
-}
-function getGames() {
-    fetch('api/query.php?query=games-by-user&id=1')
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(console.error);
+function setColor(symbol) {
+    return symbol == "X" ? conf.xcolor : conf.ocolor;
 }
 
-
-
-
-
-
-
-
-
+//jocurile sa fie pe orizonatala; 
+//butonul de reset enable si disabled; 
+//afisare  player, score, cine urmeaza; 
+//state in UI, jocul sa fie luat din baza de date la stadiul in care a ramas; 
+//acoperire grid cu blur cand se castiga sau game over; 
+//scorul afisat pt fiecare jucator;
+//utilizatorul curent cu id1 are doua jocuri in asteptare: 1 in care el trebuie sa faca mutarea, 
+// si al doilea in care sa astepte dupa jucator;
+//afisare mesaj "turn of player x, sau turn of player o";
+window.clickCell = clickCell;
+window.reset = reset;
