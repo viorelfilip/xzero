569f9bf77a540ef8ecebab519b51862c8c95ca1e
Emeric8 <emeric.lacatus24@gmail.com>
2021-11-01 11:05:32 +0200

    ++

--- data.js
+++ data.js
@@ -5,18 +5,6 @@ export function isSaving(value) {
     return saving;
 }
 
-export function saveScore(scorUser1, scorUser2, gameid) {
-    isSaving(true);
-    return fetch("/xzero/api/query.php", {
-        method: "POST",
-        body: JSON.stringify({
-            "query": `set-game-scor`,
-            scorUser1,
-            scorUser2,
-            gameid
-        })
-    }).then(res=>isSaving(false));
-}
 
 export function saveMove(cell, value, gameid) {
     isSaving(true);
